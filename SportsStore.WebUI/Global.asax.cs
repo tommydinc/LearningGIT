73eac6a48880204988b6b1e3472f89601004dc87
Tommy Dutton <tommydinc@hotmail.com>
2013-07-09 22:56:18 -0400

    day 2
    these are day 2 changes

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -28,7 +28,7 @@ namespace SportsStore.WebUI
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Product", action = "List", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -7,35 +7,35 @@ using System.Web.Routing;
 using Ninject;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using Moq;
 
 
 namespace SportsStore.WebUI.Infrastructure
 {
-    public class NinjectControllerFactory : DefaultControllerFactory
-    {
-        private IKernel ninjectKernel;
-        public NinjectControllerFactory()
-        {
-            ninjectKernel = new StandardKernel(); 
-            AddBindings();
-        }
-        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
-{
- 	return controllerType == null 
-        ? null
-        :
-        (IController)ninjectKernel.Get(controllerType);
-        }         
-        private void AddBindings() 
-{
-    //put additional bindings here
+     public class NinjectControllerFactory : DefaultControllerFactory {         
+         private IKernel ninjectKernel;         
 
-            //mock implementation of the products            
-    Mock<IProductRepository> mock = new Mock<IProductRepository>();
-    mock.Setup(m => m.Products).Returns(new List<Product> { 
-        new Product { Name = "Football", Price = 25 }, new Product { Name = "Football", Price = 25 }, new Product { Name = "Football", Price = 25 } }.AsQueryable());
-            
-            
-}
+         public NinjectControllerFactory() {             
+             ninjectKernel = new StandardKernel();             
+             AddBindings();         
+         }         
+         
+         protected override IController GetControllerInstance(RequestContext requestContext,Type controllerType) {
+             return controllerType == null
+                 ? null
+                 : (IController)ninjectKernel.Get(controllerType);         
+         }
+
+private void AddBindings() {     
+    // Mock implementation of the IProductRepository Interface    
+    Mock<IProductRepository> mock = new Mock<IProductRepository>();     
+    mock.Setup(m => m.Products).Returns(new List<Product> {        
+        new Product { Name = "Football", Price = 25 },         
+        new Product { Name = "Surf board", Price = 179 },        
+        new Product { Name = "Running shoes", Price = 95 }     
+    }.AsQueryable());     
+    ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object); }
+
+
 }
 }
